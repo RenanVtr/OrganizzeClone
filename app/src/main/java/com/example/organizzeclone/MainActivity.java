ec6a2849a3241d4a6460de14638260a72f986c10
RenanVtr <renanvitorg@hotmail.com>
2022-09-13 17:54:43 -0300

    Slider Fragment

--- app/src/main/java/com/example/organizzeclone/MainActivity.java
+++ app/src/main/java/com/example/organizzeclone/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 import com.heinrichreimersoftware.materialintro.slide.Slide;
 
@@ -15,35 +16,78 @@ public class MainActivity extends IntroActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-        addSlide((Slide) new SimpleSlide.Builder()
-                                        .title("Titulo")
-                                        .description("Descrição")
-                                        .image(R.drawable.um)
-                                        .background(android.R.color.holo_orange_light)
-                                        .build()
-        );
-        addSlide((Slide) new SimpleSlide.Builder()
-                                        .title("Titulo2")
-                                        .description("Descrição2")
-                                        .image(R.drawable.dois)
-                                        .background(android.R.color.holo_orange_light)
-                                        .build()
+
+        //BLOCO INTRO FRAGMENT
+        setButtonBackVisible(false);  //Removendo Botão Back
+        setButtonNextVisible(false);  //Removendo Botão Next
+
+
+        //Intro 1 Fragment
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
         );
 
-        addSlide((Slide) new SimpleSlide.Builder()
-                                        .title("Titulo3")
-                                        .description("Descrição3")
-                                        .image(R.drawable.tres)
-                                        .background(android.R.color.holo_orange_light)
-                                        .build()
+        //Intro 2 Fragment
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
         );
 
-        addSlide((Slide) new SimpleSlide.Builder()
-                                        .title("Titulo4")
-                                        .description("Descrição4")
-                                        .image(R.drawable.quatro)
-                                        .background(android.R.color.holo_orange_light)
-                                        .build()
+        //Intro 3 Fragment
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
         );
+        //Intro 4 Fragment
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+        //FIM DO BLOCO INTRO FRAGMENT
+
+
+        /*    CRIAÇÃO DE SLIDER SIMPLICADO ----- NÃO UTILIZADO
+        setButtonBackVisible(false);  //Removendo Botão Back
+        setButtonNextVisible(false);  //Removendo Botão Next
+
+        //Intro 1 SimpleSlider
+        addSlide(new SimpleSlide.Builder()
+                                .title("Titulo")
+                                .description("Descrição")
+                                .image(R.drawable.um)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+        //Intro 2 SimpleSlider
+        addSlide(new SimpleSlide.Builder()
+                                .title("Titulo2")
+                                .description("Descrição2")
+                                .image(R.drawable.dois)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+        //Intro 3 SimpleSlider
+        addSlide(new SimpleSlide.Builder()
+                                .title("Titulo3")
+                                .description("Descrição3")
+                                .image(R.drawable.tres)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+        //Intro 4 SimpleSlider
+        addSlide(new SimpleSlide.Builder()
+                                .title("Titulo4")
+                                .description("Descrição4")
+                                .image(R.drawable.quatro)
+                                .background(android.R.color.holo_orange_light)
+                                .build()
+        );
+        //FIM DO BLOCO SLIDER SIMPLIFICADO
+         */
     }
 }
