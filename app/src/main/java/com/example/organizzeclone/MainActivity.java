f6dd075e2d006c46dc5a409597ce4029cdb15be9
RenanVtr <renanvitorg@hotmail.com>
2022-09-13 16:55:54 -0300

    Criação do Slider

--- app/src/main/java/com/example/organizzeclone/MainActivity.java
+++ app/src/main/java/com/example/organizzeclone/MainActivity.java
@@ -13,12 +13,37 @@ public class MainActivity extends IntroActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         addSlide((Slide) new SimpleSlide.Builder()
-                                .title("Titulo")
-                                .description("Descrição")
+                                        .title("Titulo")
+                                        .description("Descrição")
+                                        .image(R.drawable.um)
+                                        .background(android.R.color.holo_orange_light)
+                                        .build()
+        );
+        addSlide((Slide) new SimpleSlide.Builder()
+                                        .title("Titulo2")
+                                        .description("Descrição2")
+                                        .image(R.drawable.dois)
+                                        .background(android.R.color.holo_orange_light)
+                                        .build()
+        );
 
+        addSlide((Slide) new SimpleSlide.Builder()
+                                        .title("Titulo3")
+                                        .description("Descrição3")
+                                        .image(R.drawable.tres)
+                                        .background(android.R.color.holo_orange_light)
+                                        .build()
+        );
+
+        addSlide((Slide) new SimpleSlide.Builder()
+                                        .title("Titulo4")
+                                        .description("Descrição4")
+                                        .image(R.drawable.quatro)
+                                        .background(android.R.color.holo_orange_light)
+                                        .build()
         );
     }
 }
